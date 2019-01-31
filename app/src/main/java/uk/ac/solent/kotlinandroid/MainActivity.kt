54439d503510a589e0e2f6c74542df7c9491037b
jenkinsmi <jenkinsmi@hotmail.com>
2019-01-31 13:02:20 +0000

    lesson 2 question 2

--- app/src/main/java/uk/ac/solent/kotlinandroid/MainActivity.kt
+++ app/src/main/java/uk/ac/solent/kotlinandroid/MainActivity.kt
@@ -9,13 +9,29 @@ import android.widget.TextView;
 import android.widget.EditText;
 import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn1.setOnClickListener { Toast.makeText(this, et1.getText(), Toast.LENGTH_SHORT).show()}
+
+        btn1.setOnClickListener {
+            Toast.makeText(this, et1.getText(), Toast.LENGTH_SHORT).show()
+            doAsync {
+                val url = URL("http://www.free-map.org.uk/course/ws/hits.php?artist=${et1.text}")
+                val response = url.readText()
+
+
+                uiThread {
+                    tv2.text = response
+                }
+            }
+        }
     }
 }
 
